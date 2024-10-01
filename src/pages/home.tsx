c574477e08b02e12305fccc82f2a3410dfb1db93
Sourina Nandy <sourina.nandy@gmail.com>
2024-10-01 18:47:27 +0200

    carousel component updated

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,17 @@
-import Footer from '../components/Footer/Footer';
-import NavBar from '../components/NavBar/NavBar';
-import './home.css';
+import Carousal from "../components/Carousal/Carousal";
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+import "./home.css";
 const Home = () => {
   return (
     <>
-      <section className='home-page'>
+      <section className="home-page">
         <NavBar />
-        <main className='home-page__main'>
+        <main className="home-page__main">
           <h1>Home</h1>
         </main>
+        <Carousal />
+
         <Footer />
       </section>
     </>
