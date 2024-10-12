9937ea08ef2c68e7cf1a959872c829a996ac984f
Loppan1 <139924795+Loppan1@users.noreply.github.com>
2024-10-12 13:25:07 +0200

    moved MovieCard to component

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { handleImageError } from '../../utils/handleImageError';
+import './MovieCard.css'
+
+interface Movie {
+    title: string;
+    year: number;
+    rating: string;
+    actors: string[];
+    genre: string;
+    synopsis: string;
+    thumbnail: string;
+    isTrending?: boolean;
+  }
+
+interface MovieCardProps {
+    movie: Movie;
+    type: string;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie, type }) => {
+
+    let cardClass = type;
+
+    return (
+        <article>
+          <Link to={`/movieview/${movie.title}`} className='movie__link'>
+            <div className={`movie-card ${cardClass}`}>
+              <img
+                src={movie.thumbnail}
+                alt={`${movie.title} thumbnail`}
+                onError={handleImageError}
+              />
+              <div className='movie-details'>
+                <p>{movie.year}</p>
+                <p>{movie.rating}</p>
+              </div>
+            </div>
+          </Link>
+        </article>
+      );
+}
+
+  export default MovieCard;
